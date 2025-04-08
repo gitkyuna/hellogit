5d3bff4cd889d8bb3a2b8eafb8effb743990d4d6
lastcoder <pkn512@gmail.com>
2025-04-08 15:24:48 +0900

    user01 modify

--- test03.py
+++ test03.py
@@ -1,7 +1,8 @@
 #test03.py
 def sayHello(a, b):
-    return 'Hello, ' + name
+    return 'Hello' + name + '!'
     
-inName = 'Tom'
-print(sayHello(inName)
+inName = 'Tom Brown'
+print(sayHello(inName))
 print("main 브랜치에서 새로운 커밋 생성")
+
